f0840d3cc233f6d048617500a54d8025615b948e
Gimhani Samarasinghe <gimhanis1996@gmail.com>
2020-11-16 19:26:50 +0530

    updated

--- src/pages/home/home-page.js
+++ src/pages/home/home-page.js
@@ -1,10 +1,57 @@
 import * as React from 'react';
 import {Text, View} from 'react-native';
+import {TextInput} from 'react-native';
+import {Button} from 'react-native';
 
 export function HomeScreen() {
   return (
+    
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
+
+      <View style={{
+        borderRadius:10,
+        borderColor:'#0558B4',
+        borderWidth:1,
+        width:200,
+        alignItems:'center',
+        marginBottom:8}}>
+      <TextInput style={{
+        height: 40}} 
+        placeholder="From " />
+      </View>
+
+      <View  style={{
+        borderRadius:10,
+        borderColor:'#0558B4',
+        borderWidth:1,
+        width:200,
+        alignItems:'center',
+        marginBottom:30}}>
+      <TextInput style={{
+            height: 40
+         }}
+        placeholder="To "
+      />
+      </View>
+         
+    <View style={{width:200}} >
+      <Button
+        title="Go"
+        color="#0558B4"
+        style={{
+          width:200,
+          height:40
+        }}
+      />
+    </View>
     </View>
   );
-};
+
+  const styles= StyleSheet.cre
+}
+
+// function HomeScreen  ()  {
+//   return <Text>Hello, I am your cat!</Text>;
+// };
+
+// export default HomeScreen;
--- src/pages/settings/settings-page.js
+++ src/pages/settings/settings-page.js
@@ -1,10 +1,26 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image, ScrollView} from 'react-native';
+import MapViewDirections from 'react-native-maps-directions';
+import {MapView, PROVIDER_GOOGLE} from 'react-native-maps-directions';
 
 export function SettingsScreen() {
+  let GOOGLE_MAPS_APIKEY = 'AIzaSyDYNU9mpalnAUDdGMlml3E7ZUwNCxQPrjM';
+  let origin = {latitude: 6.9769, longitude: 80.052843};
+  let destination = {latitude: 6.970989, longitude: 80.056788};
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
-    </View>
+    <MapView
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}>
+      provider={PROVIDER_GOOGLE}
+      <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_MAPS_APIKEY}
+      />
+    </MapView>
   );
-};
+}
